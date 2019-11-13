e88dd609709e7cc268604940b7304f1084c4915c
Vishwa Perera <pereravishwa061@gmail.com>
2019-11-13 16:08:05 -0500

    update

--- Back-End/Back End/src/back_end_package/Person.java
+++ Back-End/Back End/src/back_end_package/Person.java
@@ -1,13 +1,11 @@
 package back_end_package;
 
-import java.util.Comparator;
 import java.util.List;
 
-public abstract class Person implements Comparable<Person> {
-	protected String name;
-	protected String email;
-	protected String id;
-	public static final Comparator<Person> CMP = null;
+public abstract class Person {
+	private String name;
+	private String email;
+	private int id;
 
 	public Person(List<String> list) {
 		if (list.size() != 3) {
@@ -16,7 +14,7 @@ public abstract class Person implements Comparable<Person> {
 		}
 		this.name = list.get(0);
 		this.email = list.get(1);
-		this.id = list.get(2);
+		this.id = Integer.parseInt(list.get(2));
 	}
 
 	public String getName() {
@@ -27,13 +25,8 @@ public abstract class Person implements Comparable<Person> {
 		return this.email;
 	}
 
-	public String getID() {
+	public int getID() {
 		return this.id;
-	}
-
-	@Override
-	public int compareTo(Person other) {
-		return this.id.compareTo(other.id);
 	}
 
 	public static void main(String[] args) {
